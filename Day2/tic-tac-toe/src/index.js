5480633e91082a99227e58ef13882e4bc66ebe4b
jamaltan31 <jamaltan31@gmail.com>
2019-10-15 10:05:38 +0800

    update tictactor - day2

--- Day2/tic-tac-toe/src/index.js
+++ Day2/tic-tac-toe/src/index.js
@@ -1,3 +1,14 @@
+/* Steps to build tictactoe game : `https://reactjs.org/tutorial/tutorial.html`
+  > create 9 boxes manually using JSX & CSS on separate file 
+  > name each boxes and actually print out the index number (0 - 8) on each boxes using renderSquare() self build function & this.props.value (result : 9 boxes with numbering 0 to 8)
+  > put onClick on every squares, and shows alert() messages when clicked on any of them (result : 9 boxes with numbering 0 to 8 & alert pop up everytime i click any of the small boxes)
+  > clear each boxes from displaying the index number by defining `this.state = {value: null}`
+  > put X on selected box by setting `OnClick={() => this.setState({value: "X"})}`
+  > set 'board' component to start game with 9 null spaces using `this.state = {squares: Array(9).fill(null),};`
+  
+  
+  */
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -5,13 +16,35 @@ import "./index.css";
 // 1st class : Square - 1 small square
 class Kotak extends React.Component {
   // converts whatever inside render() to be display to website
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null // tell react to start the game with empty boxes, NOT number of each array
+    };
+  }
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          this.setState({ value: "X" }); // onClick used to update the clicked squares with the new state using setState()
+        }}
+      >
+        {this.state.value}
+      </button>
+    );
   }
 }
 
 // 2nd class : Board - 9 small squares
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null) // create new state here called `squares` set 'board' component to start game with 9 null spaces
+    };
+  }
+
   renderKotak(i) {
     return <Kotak value={i} />; // putting number on each box
   }
